fb4a15ceacedf86b8980627ea5e74e67320996e4
heeropunjabi <heeropunjabi@gmail.com>
2019-06-05 14:43:36 +0530

    appolo  Hero

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import LinkList from './LinkList'
+import Header from './Header'
+import CreateLink from './CreateLink'
+import { Switch, Route } from 'react-router-dom'
+
+
+class App extends Component {
+  render() {
+    return (<div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>)
+  }
+}
+
+export default App
